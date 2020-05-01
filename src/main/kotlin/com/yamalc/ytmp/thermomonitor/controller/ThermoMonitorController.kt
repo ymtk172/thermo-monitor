66b6b9c612f2264398e0f55685171d639878b996
YAMATO Keiichi <ymtk172@gmail.com>
2020-05-01 10:36:01 +0900

    modify to get username from authantication object

--- src/main/kotlin/com/yamalc/ytmp/thermomonitor/controller/ThermoMonitorController.kt
+++ src/main/kotlin/com/yamalc/ytmp/thermomonitor/controller/ThermoMonitorController.kt
@@ -4,17 +4,13 @@ import com.yamalc.ytmp.thermomonitor.component.ThermoComponent
 import com.yamalc.ytmp.thermomonitor.fw.security.LoginUser
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.ui.set
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/")
 class ThermoMonitorController(private val thermoCompo: ThermoComponent) {
     @RequestMapping("/top")
-    fun top(model: Model, @AuthenticationPrincipal loginUser: LoginUser): String {
-        val userName = "user1"
-        model["username"] = userName
+    fun top(@AuthenticationPrincipal loginUser: LoginUser): String {
         return "top"
     }
 }
