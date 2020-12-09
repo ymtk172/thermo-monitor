6a3118b9a6d4d3e009c3fdb9204d65791d07f9f5
yamato.keiichi <yamato.keiichi@tis.co.jp>
2020-12-09 19:45:03 +0900

    refactored

--- src/main/kotlin/com/yamalc/ytmp/thermomonitor/controller/ThermoMonitorController.kt
+++ src/main/kotlin/com/yamalc/ytmp/thermomonitor/controller/ThermoMonitorController.kt
@@ -1,12 +1,11 @@
 package com.yamalc.ytmp.thermomonitor.controller
 
-import com.yamalc.ytmp.thermomonitor.component.ThermoComponent
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/")
-class ThermoMonitorController(private val thermoCompo: ThermoComponent) {
+class ThermoMonitorController {
 
     @RequestMapping("/top")
     fun top(): String {
